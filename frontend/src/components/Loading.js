fcf446562820a835a90d89b503699915bce358cd
David de la Calle Cerezo <david.dlc.cerezo@gmail.com>
2017-12-09 22:42:48 +0100

    New component Loading

---
+++ frontend/src/components/Loading.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default () => (
+  <section className="sg-section">
+    <div className="Loading">
+      <div className="Loading-wrapper">
+        <div className="Loading-spinner"></div>
+        <p className="Loading-text">
+          Cargando contenido.<br/>
+          Por favor, espera unos instantes.
+        </p>
+      </div>
+    </div>
+  </section>
+);
